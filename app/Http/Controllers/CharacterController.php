05c2b134a4a5a6176e16e1f8076ea9385deda2ec
Ansell Maximilian <ansellmaximilian@gmail.com>
2021-08-07 21:37:11 +0700

    basic character api

---
+++ app/Http/Controllers/CharacterController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Character;
+use Illuminate\Http\Request;
+
+class CharacterController extends Controller
+{
+    public function index()
+    {
+        return Character::all();
+    }
+
+    public function show(Character $character)
+    {
+        return $character;
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'gender' => 'in:male,female|nullable'
+        ]);
+
+        return Character::create($request->all());
+    }
+
+    public function destroy($id)
+    {
+        return Character::destroy($id);
+    }
+
+    public function update(Request $request, Character $character)
+    {
+        $request->validate([
+            'name' => 'required',
+            'gender' => 'in:male,female|nullable'
+        ]);
+
+        return 1;
+
+        return $character;
+
+        return $character->update($request->all());
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CharacterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('/characters', [CharacterController::class, 'index']);
+Route::get('/characters/{character}', [CharacterController::class, 'show']);
+Route::post('/characters', [CharacterController::class, 'store']);
+Route::delete('/characters/{character}', [CharacterController::class, 'destroy']);
+Route::put('/characters/{character}', [CharacterController::class, 'update']);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
