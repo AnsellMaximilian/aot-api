61d686ec1e137a4dd4135814ccb3b46550595f45
Ansell Maximilian <ansellmaximilian@gmail.com>
2021-08-08 17:51:47 +0700

    tokens routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\CharacterController;
+use App\Http\Controllers\Auth\TokenController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,17 +18,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// PROTECTED ROTUES
+Route::middleware('auth:sanctum')->group(function(){
+    Route::post('/characters', [CharacterController::class, 'store']);
+    Route::delete('/characters/{character}', [CharacterController::class, 'destroy']);
+    Route::put('/characters/{character}', [CharacterController::class, 'update']);
+});
+
 Route::get('/characters', [CharacterController::class, 'index']);
 Route::get('/characters/{character}', [CharacterController::class, 'show']);
-Route::post('/characters', [CharacterController::class, 'store']);
-Route::delete('/characters/{character}', [CharacterController::class, 'destroy']);
-Route::put('/characters/{character}', [CharacterController::class, 'update']);
 
 // AUTH
 Route::post('/register', [RegisterController::class, 'register']);
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout'])->middleware('auth:sanctum');
 
+
+// TOKEN ROUTES
+Route::middleware('auth:sanctum')->prefix('tokens')->group(function(){
+    Route::post('/remove-all', [TokenController::class, 'removeAllTokens']);
+    Route::get('/', function(){
+        return 'test';
+    });
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
